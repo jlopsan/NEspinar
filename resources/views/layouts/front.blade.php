69e3b55546b92721a8af0c70bc7b4bbdbc05192c
Ajax-16 <davidbernardezluque7618@gmail.com>
2024-01-25 14:28:07 +0100

    quitado cdn bootstrap

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -15,8 +15,6 @@
     <link href="https://fonts.googleapis.com/css?family={{$opciones['tipografia1']}}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family={{$opciones['tipografia2']}}" rel="stylesheet">
     <!-- Mi fuente-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="/css/bootstrap.css" rel="stylesheet" type="text/css" />
     <link href="/css/frontStyles.css" rel="stylesheet" type="text/css" />
     @yield('css')
