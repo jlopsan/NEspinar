213bd074b5a8956f48ee04b13dc32cee0adb2958
Juan <juanloperasanchez@gmail.com>
2024-01-29 12:06:14 +0100

    Actualizada la sintaxis de las rutas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\FrontController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ItemsController;
+use App\Http\Controllers\CategoriasController;
+use App\Http\Controllers\ProductosController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,38 +18,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'FrontController@index')->name('home');
+Route::get('/', [FrontController::class, 'index'])->name('home');
 
-Route::get('/categorias/get_items/{id_categoria}', 'CategoriasController@get_items');
+Route::get('/categorias/get_items/{id_categoria}', [CategoriasController::class, 'get_items']);
 
 Route::resource('productos', 'ProductosController');
 Route::resource('categorias', 'CategoriasController');
 Route::resource('etiquetas', 'EtiquetasController');
-Route::get('/items/category/{id}', 'ItemsController@index')->name('items.indexPorCategoria');
-Route::get('/categorias/category/{id}', 'CategoriasController@index')->name('items.Categoria');
-Route::get('/items/cambiar-orden/{id}/{orden}/{cantidad}', 'ItemsController@cambiarOrden')->name('items.changeOrder');
-Route::get('/categorias/cambiar-orden/{id}/{orden}/{cantidad}', 'CategoriasController@cambiarOrden')->name('categorias.changeOrder');
-Route::get('/items/destacar/{id}/{destacado}', 'ItemsController@destacar')->name('items.destacar');
+Route::get('/items/category/{id}', [ItemsController::class, 'index'])->name('items.indexPorCategoria');
+Route::get('/categorias/category/{id}', [CategoriasController::class, 'index'])->name('items.Categoria');
+Route::get('/items/cambiar-orden/{id}/{orden}/{cantidad}', [ItemsController::class, 'cambiarOrden'])->name('items.changeOrder');
+Route::get('/categorias/cambiar-orden/{id}/{orden}/{cantidad}', [CategoriasController::class, 'cambiarOrden'])->name('categorias.changeOrder');
+Route::get('/items/destacar/{id}/{destacado}', [ItemsController::class, 'destacar'])->name('items.destacar');
 Route::resource('items', 'ItemsController');
 Route::resource('opciones', 'OpcionesController');
 Route::resource('imagenes', 'ImagenesController');
 Route::resource('usuarios', 'UsersController');
 
 // Rutas para los buscadores
-Route::get('buscadorBack', 'ProductosController@buscadorProductos')->name('buscadorBack');
-Route::get('/categoria/{id}', 'FrontController@mostrarCategorias')->name('productoPorCategoria');
-Route::get('buscadorFront', 'FrontController@buscadorGeneral')->name('buscadorFront');
-Route::get('buscador', 'FrontController@vistaBuscador')->name('vistaBuscador');
+Route::get('buscadorBack', [ProductosController::class, 'buscadorProductos'])->name('buscadorBack');
+Route::get('/categoria/{id}', [FrontController::class, 'mostrarCategorias'])->name('productoPorCategoria');
+Route::get('buscadorFront', [FrontController::class, 'buscadorGeneral'])->name('buscadorFront');
+Route::get('buscador', [FrontController::class, 'vistaBuscador'])->name('vistaBuscador');
 
-Route::match(['get', 'post'],'buscadorPorcampos', 'FrontController@buscadorPorCampos')->name('buscadorPorCampos');
+Route::match(['get', 'post'],'buscadorPorcampos', [FrontController::class, 'buscadorPorCampos'])->name('buscadorPorCampos');
 
-Route::get('/vistaPorItemDestacado/{idCategoria}/{idItem}/{valorItem}', 'FrontController@vistaPorItemDestacado')->name('front.porItemDestacado');
+Route::get('/vistaPorItemDestacado/{idCategoria}/{idItem}/{valorItem}', [FrontController::class, 'vistaPorItemDestacado'])->name('front.porItemDestacado');
 
 // Rutas para las páginas de información legal
-Route::get('acerca_de', 'FrontController@acercaDe')->name('acerca_de');
-Route::get('politica_privacidad', 'FrontController@politicaPrivacidad')->name('politica_privacidad');
-Route::get('terminos_uso', 'FrontController@terminosUso')->name('terminos_uso');
-Route::get('politica_cookies', 'FrontController@politicaCookies')->name('politica_cookies');
+Route::get('acerca_de', [FrontController::class, 'acercaDe'])->name('acerca_de');
+Route::get('politica_privacidad', [FrontController::class, 'politicaPrivacidad'])->name('politica_privacidad');
+Route::get('terminos_uso', [FrontController::class, 'terminosUso'])->name('terminos_uso');
+Route::get('politica_cookies', [FrontController::class, 'politicaCookies'])->name('politica_cookies');
 
 Route::get('/dashboard', function () {
     return view('productos.all');
