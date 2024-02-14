a5ad3fcab06336507e2dbd58ce42f4c48ed902c1
Ajax-16 <davidbernardezluque7618@gmail.com>
2024-02-14 14:25:37 +0100

    zoom integrado

--- public/js/zoom.js
+++ public/js/zoom.js
@@ -6,9 +6,9 @@ function zoom(e) {
         offsetY = e.offsetY;
     } else {
         offsetX = e.touches[0].pageX;
-        offsetY = e.touches[0].pageY; // Corregir aquí
+        offsetY = e.touches[0].pageY;
     }
     var x = offsetX / zoomer.offsetWidth * 100;
-    var y = offsetY / zoomer.offsetHeight * 100; // Corregir aquí
+    var y = offsetY / zoomer.offsetHeight * 100;
     zoomer.style.backgroundPosition = x + '% ' + y + '%';
 }
