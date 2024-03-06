bf24acf0976c945b2aa643650fdb07f6cfd0ef4e
Ajax-16 <davidbernardezluque7618@gmail.com>
2024-03-06 20:16:16 +0100

    añadido efecto zoom carousel al hacer hover

--- public/js/scrollAnimation.js
+++ public/js/scrollAnimation.js
@@ -1,27 +1,55 @@
 document.addEventListener("DOMContentLoaded", () => {
     var modals = document.querySelectorAll('.modal');
 
+    // Definir funciones de flecha para addScrolled y removeScrolled
+    const addScrolled = (imgs, title) => {
+        imgs.forEach(img => img.classList.add('scrolled'));
+        title.classList.add('title-scrolled');
+    };
+
+    const removeScrolled = (imgs, title) => {
+        imgs.forEach(img => img.classList.remove('scrolled'));
+        title.classList.remove('title-scrolled');
+    };
+
     function applyScrollLogic(modal) {
         var imgs = modal.querySelectorAll('.img-wrapper img');
+        var carousel = modal.querySelector('.carousel');
         var title = modal.querySelector('h2');
-        var scrollThreshold = 10;
+        var scrollThreshold = 50;
         var isScrolled = false;
 
         function scrollHandler() {
             if (!isScrolled && modal.scrollTop >= scrollThreshold) {
                 isScrolled = true;
-                imgs.forEach(img => img.classList.add('scrolled'));
-                title.classList.add('title-scrolled');
-            } else if (isScrolled && modal.scrollTop < scrollThreshold) {
+                addScrolled(imgs, title);
+                carousel.addEventListener('mouseover', mouseOverHandler);
+                carousel.addEventListener('mouseout', mouseOutHandler);
+            } else if (modal.scrollTop < scrollThreshold) {
                 isScrolled = false;
-                imgs.forEach(img => img.classList.remove('scrolled'));
-                title.classList.remove('title-scrolled');
+                removeScrolled(imgs, title); 
+                carousel.removeEventListener('mouseover', mouseOverHandler);
+                carousel.removeEventListener('mouseout', mouseOutHandler);
             }
+        }
+
+        function mouseOverHandler() {
+            removeScrolled(imgs, title);
+        }
+
+        function mouseOutHandler() {
+            addScrolled(imgs, title);
         }
 
         modal.addEventListener('scroll', scrollHandler);
 
-        modal.scrollTop = 1;
+        // Eliminar los event listeners cuando se cierra el modal
+        modal.addEventListener('hidden.bs.modal', () => {
+            modal.removeEventListener('scroll', scrollHandler);
+            carousel.removeEventListener('mouseover', mouseOverHandler);
+            carousel.removeEventListener('mouseout', mouseOutHandler);
+        });
+
         scrollHandler(); 
     }
 
@@ -30,7 +58,4 @@ document.addEventListener("DOMContentLoaded", () => {
             applyScrollLogic(modal);
         });
     });
-
-
-
 });
